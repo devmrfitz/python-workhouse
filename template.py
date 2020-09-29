d301e8abc56f3db351ea00eb267d8f455de2dfdd
Fitz Watson <58381523+devmrfitz@users.noreply.github.com>
2020-09-29 20:25:29 +0530

    Create template.py

---
+++ template.py
@@ -0,0 +1,27 @@
+#Default template for python in competitive programming
+
+#template begins
+##############################################################
+# import libraries for input/ output handling
+# on generic level
+import atexit, io, sys
+ 
+# A stream implementation using an in-memory bytes 
+# buffer. It inherits BufferedIOBase.
+buffer = io.BytesIO()
+sys.stdout = buffer
+ 
+# print via here
+@atexit.register
+def print():
+    sys.__stdout__.write(buffer.getvalue())
+
+def get_ints(): return map(int, sys.stdin.readline().strip().split())
+#Use: a,b,c,d = get_ints()
+
+
+def get_list(): return list(map(int, sys.stdin.readline().strip().split()))
+#Use: Arr = get_list()
+
+##############################################################
+#template ends
