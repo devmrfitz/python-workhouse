846558b30698223f843e581910e3b1c981ae4115
Fitz Watson <58381523+devmrfitz@users.noreply.github.com>
2020-09-29 21:08:48 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,26 +1,11 @@
-#Default template for python in competitive programming
-
-#template begins
-##############################################################
-# import libraries for input/ output handling
-# on generic level
-import atexit, io, sys
- 
-# A stream implementation using an in-memory bytes 
-# buffer. It inherits BufferedIOBase.
-buffer = io.BytesIO()
-sys.stdout = buffer
- 
-# print via here
-@atexit.register
-def write():
-    sys.__stdout__.write(buffer.getvalue())
-
-import sys
-def get_ints(): return map(int, sys.stdin.readline().strip().split())
-#Use: a,b,c,d = get_ints()
-def get_list(): return list(map(int, sys.stdin.readline().strip().split()))
-#Use: Arr = get_list()
-
-##############################################################
-#template ends
+test_cases=int(raw_input())
+for i in range(test_cases):
+    n=int(raw_input())
+    arr=raw_input().split()
+    sum=0
+    for j in range(n):
+        sum+=arr[j]
+    if (sum>=0):
+        print('YES')
+    else:
+        print('NO')
