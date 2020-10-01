fe70e289155751bca34c1a4a4dfa4f0cc2abfa2f
Fitz Watson <58381523+devmrfitz@users.noreply.github.com>
2020-10-01 17:59:13 +0530

    Update template.py

--- template.py
+++ template.py
@@ -4,12 +4,18 @@
 ##############################################################
 import sys
 
+
 def get_ints(): return map(int, sys.stdin.readline().strip().split())
 #Use: a,b,c,d = get_ints()
 
 
 def get_list(): return list(map(int, sys.stdin.readline().strip().split()))
 #Use: Arr = get_list()
+
+
+def input():
+  return sys.stdin.readline().strip()
+
 
 ##############################################################
 #template ends
