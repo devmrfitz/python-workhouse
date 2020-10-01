c76ca3ed2a5379f08da3f5bf0e24a590d5e1151c
Fitz Watson <58381523+devmrfitz@users.noreply.github.com>
2020-10-01 15:37:19 +0530

    Update template.py

--- template.py
+++ template.py
@@ -2,19 +2,7 @@
 
 #template begins
 ##############################################################
-# import libraries for input/ output handling
-# on generic level
-import atexit, io, sys
- 
-# A stream implementation using an in-memory bytes 
-# buffer. It inherits BufferedIOBase.
-buffer = io.BytesIO()
-sys.stdout = buffer
- 
-# print via here
-@atexit.register
-def print():
-    sys.__stdout__.write(buffer.getvalue())
+import sys
 
 def get_ints(): return map(int, sys.stdin.readline().strip().split())
 #Use: a,b,c,d = get_ints()
